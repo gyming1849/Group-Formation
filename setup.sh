45c826da03f141a2f55447630315f95deedd5b48
hamed <amzhamed@gmail.com>
2024-01-25 14:02:23 -0800

    update setup

--- setup.sh
+++ setup.sh
@@ -1,10 +1,56 @@
 #!/bin/bash
-sudo apt update -y
-if ! command -v pip3 &> /dev/null
-then
-    echo "pip3 could not be found"
-    echo "installing pip3 ..."
-    sudo apt install python3-pip -y
-fi
+
+# Function to install Python3 and Pip3 on Ubuntu
+install_ubuntu() {
+    sudo apt update -y
+    if ! command -v python3 &> /dev/null || ! command -v pip3 &> /dev/null
+    then
+        echo "Python3 and/or pip3 could not be found"
+        echo "Installing Python3 and pip3 ..."
+        sudo apt install python3 python3-pip -y
+    fi
+}
+
+# Function to install Python3 and Pip3 on Windows
+install_windows() {
+    if ! command -v python &> /dev/null || ! command -v pip &> /dev/null
+    then
+        echo "Python and/or pip could not be found"
+#        echo "Installing Python and pip ..."
+        # Chocolatey:
+        # choco install python -y
+        # Scoop:
+        # scoop install python
+        echo "Please install Python and pip manually."
+        exit 1
+    fi
+}
+
+# Function to install Python3 and Pip3 on MacOS
+install_mac() {
+    if ! command -v python3 &> /dev/null || ! command -v pip3 &> /dev/null
+    then
+        echo "Python3 and/or pip3 could not be found"
+#        echo "Installing Python3 and pip3 ..."
+        # Homebrew:
+        # brew install python
+        # MacPorts:
+        # port install python3
+        echo "Please install Python and pip manually."
+        exit 1
+    fi
+}
+
+# Detect the operating system and call the appropriate installation function
+case "$(uname -s)" in
+    Linux*)     install_ubuntu ;;
+    Darwin*)    install_mac ;;
+    CYGWIN*)    install_windows ;;
+    MINGW*)     install_windows ;;
+    *)          echo "Unsupported operating system" ;;
+esac
+
+# Install Python packages using pip
 pip3 install -r requirements.txt
-echo "now run python3 server.py"
+
+echo "Now run python3 server.py"
